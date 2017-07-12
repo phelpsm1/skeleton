da78a4be20cec44e0a96d6eab7a7a7ae17d553bd
Jason S Morris <jason.morris@intel.com>
2017-07-12 11:42:45 -0700

    added app:offline and app:online tasks

--- gulp/gulp.app.js
+++ gulp/gulp.app.js
@@ -7,24 +7,6 @@ const util = require('gulp-util');
 const config = require('../config.json');
 const env = require('./env.js');
 
-const scriptfile = './gulp/jarvis.ps1';
-
-const psconfig = {
-  debugMsg: false,
-  executionPolicy: 'Bypass',
-  noProfile: true
-};
-
-const web = config[env.getName()].web;
-
-const pscmdconfig = [
-  {name: 'TRANS'},
-  {env: env.getName()},
-  {servers: web.servers},
-  {apppool: web.apppool},
-  {site: web.site}
-];
-
 gulp.task('app:down', function() {
   const help = `, e.g. gulp app:down -e dev`;
 
@@ -33,21 +15,7 @@ gulp.task('app:down', function() {
     return;
   }
 
-  let ps = new shell(psconfig);
-
-  ps.addCommand(`${scriptfile} Bring-Down`, pscmdconfig);
-
-  return ps
-    .invoke()
-    .then(output => {
-      util.log(`${output}`);
-      ps.dispose();
-    })
-    .catch(err => {
-      util.log(util.colors.red(`Unexpected exception occurred:`));
-      util.log(util.colors.red(err));
-      ps.dispose();
-    });
+  return execute('Bring-Down');
 });
 
 gulp.task('app:up', function() {
@@ -58,9 +26,52 @@ gulp.task('app:up', function() {
     return;
   }
 
-  let ps = new shell(psconfig);
+  execute('Bring-Up');
+});
 
-  ps.addCommand(`${scriptfile} Bring-Up`, pscmdconfig);
+gulp.task('app:offline', function () {
+  const help = `, e.g. gulp app:offline -e dev`;
+
+  if (!env.isEnvironmentDefined()) {
+    util.log(util.colors.red(`Environment not specified`, help));
+    return;
+  }
+
+  return execute('Take-Offline');
+});
+
+gulp.task('app:online', function () {
+  const help = `, e.g. gulp app:online -e dev`;
+
+  if (!env.isEnvironmentDefined()) {
+    util.log(util.colors.red(`Environment not specified`, help));
+    return;
+  }
+
+  return execute('Take-Online');
+});
+
+function execute(command) {
+  const web = config[env.getName()].web;
+  const name = 'TRANS';
+
+  // todo: (jmorris2) need better way to config these values
+  const pscmdconfig = [
+    {name: name},
+    {env: env.getName()},
+    {servers: web.servers},
+    {apppool: web.apppool},
+    {site: web.site},
+    {app_offline_dest: `capital$\\${name}\\${env.getName()}\\current\\Web`}
+  ];
+
+  let ps = new shell({
+    debugMsg: false,
+    executionPolicy: 'Bypass',
+    noProfile: true
+  });
+
+  ps.addCommand(`./gulp/jarvis.ps1 ${command}`, pscmdconfig);
 
   return ps
     .invoke()
@@ -73,29 +84,7 @@ gulp.task('app:up', function() {
       util.log(util.colors.red(err));
       ps.dispose();
     });
-});
-
-// take the site offline without taking the pool down
-// gulp.task('app:offline', function () {
-//   var deferred = Q.defer();
-//
-//   common.checkEnvironment('app:offline');
-//
-//   shelljs.exec('jarvis.cmd take '+process.env.target+' offline');
-//   deferred.resolve();
-//
-//   return deferred.promise;
-// });
-
-// bring the site back online
-// gulp.task('app:online', function () {
-//   var deferred = Q.defer();
-//
-//   shelljs.exec('jarvis.cmd take '+process.env.target+' online');
-//   deferred.resolve();
-//
-//   return deferred.promise;
-// });
+}
 
 // recycle the app pool for the given environment
 // gulp.task('app:recycle', function () {
