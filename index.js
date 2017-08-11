c390e08a703e1c1c738b56cc7851052916e3b702
Jason S Morris <jason.morris@intel.com>
2017-08-11 08:33:42 -0700

    moved web project to ccsd/example
    changes to make this a npm module

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const env = require('./gulp/env.js')
 const files = require('./gulp/files.js')
 const mssql = require('./gulp/mssql')
 
+require('./gulp/gulp.app.js')
+require('./gulp/gulp.build.js')
+require('./gulp/gulp.db.js')
+require('./gulp/gulp.local.js')
+
 module.exports = {
   Env: env,
   Files: files,
