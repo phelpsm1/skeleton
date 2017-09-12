a7c2338a18acc0ee5b9a2f5c38761435eebc2638
Jason S Morris <jason.morris@intel.com>
2017-09-12 15:00:35 -0700

    add gulp tasks to clean releases folder (keep last five) and clean logs (keep anything modified less than 30 days ago)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mssql = require('./lib/mssql')
 
 require('./lib/gulp.app.js')
 require('./lib/gulp.build.js')
+require('./lib/gulp.clean.js')
 require('./lib/gulp.config.js')
 require('./lib/gulp.db.js')
 require('./lib/gulp.local.js')
