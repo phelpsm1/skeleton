1d73184c314f4b509d93080cc91cff6781b8a379
Jason S Morris <jason.morris@intel.com>
2017-10-24 10:38:45 -0700

    feat(smtp): SMTP is publicly available to send mail messages BREAKING CHANGE: Renaming of several tasks notify:restore:email to restore:notify:email notify:deploy to deploy:notify notify:deploy:log to deploy:notify:log notify:deploy:email to deploy:notify:email notify:deploy:nr to deploy:notify:nr

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 const env = require('./lib/env.js')
 const files = require('./lib/files.js')
-const data = require('./lib/data')
+const data = require('./lib/data.js')
+const smtp = require('./lib/smtp.js')
 
 require('./lib/gulp.app.js')
 require('./lib/gulp.build.js')
@@ -10,10 +11,12 @@ require('./lib/gulp.clean.js')
 require('./lib/gulp.config.js')
 require('./lib/gulp.sql.js')
 require('./lib/gulp.local.js')
-require('./lib/gulp.notify.js')
+require('./lib/gulp.deploy.js')
+require('./lib/gulp.restore.js')
 
 module.exports = {
   Env: env,
   Files: files,
-  Data: data
+  Data: data,
+  Smtp: smtp
 }
