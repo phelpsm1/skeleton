4d84e59bd7a902b86f436b3a826baa628032fc0d
Jason S Morris <jason.morris@intel.com>
2019-01-03 11:49:45 -0700

    chore: update gulp and use gulp.series and gulp.parallel and remove run-sequence

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,27 @@ const env = skeleton.Env
 const files = skeleton.Files
 const mssql = skeleton.Data.mssql
 
-gulp.task('example', [], () => {
-  log.info(colors.green('Example gulp task'))
+gulp.task('a', (done) => {
+  log.info(colors.green('gulp task a'))
+  done()
 })
+
+gulp.task('b', (done) => {
+  log.info(colors.green('gulp task b'))
+  done()
+})
+
+gulp.task('c1', (done) => {
+  log.info(colors.green('gulp task c1'))
+  done()
+})
+
+gulp.task('c2', (done) => {
+  log.info(colors.green('gulp task c2'))
+  done()
+})
+
+gulp.task('example', gulp.series('a', 'b', gulp.parallel('c1', 'c2'), (done) => {
+  log.info(colors.green('example gulp task'))
+  done()
+}))
