10138957649f1dd869a7da90d1d52f591d321870
Jason S Morris <jason.morris@intel.com>
2020-06-30 16:38:59 -0700

    feat: add commit messages to deployment notifications

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,11 @@ const log = require('fancy-log')
 const skeleton = require('./index')
 
 const env = skeleton.Env
-const files = skeleton.Files
-const mssql = skeleton.Data.mssql
+// const files = skeleton.Files
+// const mssql = skeleton.Data.mssql
+const git = skeleton.Source.git
+// const gitlab = skeleton.Source.gitlab
+// const gitlocal = skeleton.Source.gitlocal
 
 const a = (done) => {
   log.info(colors.green('gulp task a'))
@@ -38,7 +41,7 @@ function checkGuards (plugin) {
   function checkRandom (done) {
     const random = Math.random()
 
-    log.info(colors.green(random))
+    log.info(colors.green(`${random}`))
 
     if (random > 0.5) {
       throw new GulpError({ plugin: plugin, message: 'There was an error', showStack: true })
@@ -58,6 +61,30 @@ function checkGuards (plugin) {
 
 const example = series(checkGuards('example'), a, b, parallel(c1, c2), (done) => {
   log.info(colors.green('example gulp task'))
+
+  const conf = env.getArgs()
+
+  const start = 'b90ee53a53f1d4479f4db2cf49eabc35b99e2c0b'
+  const end = 'fdfba5428e9f2fab315b7b6ddd75f5904357b721'
+
+  const print = (c) => {
+    log.info(c)
+  }
+
+  print(conf)
+
+  git.commits.fetch(start, end, { pid: env.pkg.gitlabprojectid, token: conf.gitlab.token })
+    .then(print)
+    .catch((err) => log.error(err))
+
+  // gitlab.commits.fetch(start, end, { pid: env.pkg.gitlabprojectid, token: conf.gitlab.token })
+  //   .then(print)
+  //   .catch((err) => log.error(err))
+
+  // gitlocal.commits.fetch(start, end)
+  //   .then(print)
+  //   .catch((err) => log.error(err))
+
   done()
 })
 
